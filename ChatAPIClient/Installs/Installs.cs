1a183cbb083e0817580a232bd09efd633c44e864
Stephen Burgess <stephen_burgess@hotmail.com>
2017-07-27 21:17:33 +0100

    Added checkin to client api

--- ChatAPIClient/Installs/Installs.cs
+++ ChatAPIClient/Installs/Installs.cs
@@ -69,9 +69,9 @@ namespace MednaNetAPIClient.Installs
             return await GetCurrentInstall(false);
         }
 
-        public void CheckinInstall()
+        public async void CheckinInstall()
         {
-
+            HttpResponseMessage response = await client.GetAsync("api/v1/installs");
         }
     }
 }
