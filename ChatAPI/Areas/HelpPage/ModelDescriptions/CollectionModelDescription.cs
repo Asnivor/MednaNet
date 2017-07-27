46a6b9d498ee785e83f4af90923d95fbea3f9990
Stephen Burgess <stephen_burgess@hotmail.com>
2017-07-27 16:08:38 +0100

    Removed redundant files

--- ChatAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ChatAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
