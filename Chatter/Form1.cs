84843b30c9a4644b68b783444285dbc8a52e83bd
Stephen Burgess <stephen_burgess@hotmail.com>
2017-07-27 22:05:55 +0100

    Removed broken implementation to allow solution to build.

--- Chatter/Form1.cs
+++ Chatter/Form1.cs
@@ -18,39 +18,39 @@ namespace Chatter
 
         public Form1()
         {
-            InitializeComponent();
-            client.start("");
+            /*InitializeComponent();
+            client.start("");*/
             
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            var message = new MednaNetAPIClient.Data.Messages();
+            /*var message = new MednaNetAPIClient.Data.Messages();
             message.code = "test";
-            message.message = createMessagebox.Text;
-            
-           
+            message.message = createMessagebox.Text;*/
 
-            await client.CreateMessage(message);
+
+
+            /*await client.CreateMessage(message);*/
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            var group = new MednaNetAPIClient.Data.Groups();
+            /*var group = new MednaNetAPIClient.Data.Groups();
             group.groupDescription = "Group description";
             group.groupName = "This is the group Name";
             group.groupOwner = 2;
 
-            await client.CreateGroup(group);
+            await client.CreateGroup(group);*/
         }
 
-        
+
 
         private async void createInstall(object sender, EventArgs e)
         {
-            string installKey = await client.CreateInstall();
+            /* string installKey = await client.CreateInstall();
 
-            MessageBox.Show(installKey);
+             MessageBox.Show(installKey);*/
         }
 
         private void button3_Click_1(object sender, EventArgs e)
@@ -60,26 +60,26 @@ namespace Chatter
 
         private async void populateInstallsDropDown(object sender, EventArgs e)
         {
-            List<MednaNetAPIClient.Data.Installs> installs =  await client.GetInstalls();
+            /*List<MednaNetAPIClient.Data.Installs> installs =  await client.GetInstalls();
 
             installsDropdown.Items.Clear();
 
             foreach (var install in installs)
             {
                 installsDropdown.Items.Add(install.code);
-            }
+            }*/
         }
 
         private async void populateGroupsDropdown(object sender, EventArgs e)
         {
-            List<MednaNetAPIClient.Data.Groups> groups = await client.GetGroups();
+            /*List<MednaNetAPIClient.Data.Groups> groups = await client.GetGroups();
 
             groupsDropdown.Items.Clear();
 
             foreach (var group in groups)
             {
                 groupsDropdown.Items.Add(group.groupName);
-            }
+            }*/
         }
 
         private async void createGroup(object sender, EventArgs e)
@@ -87,20 +87,20 @@ namespace Chatter
             //Get install
 
 
-            var group = new MednaNetAPIClient.Data.Groups();
+            /*var group = new MednaNetAPIClient.Data.Groups();
             group.groupDescription = "";
             group.groupName = groupNameText.Text;
             group.groupOwner = currentInstall.id;
 
-            await client.CreateGroup(group);
+            await client.CreateGroup(group);*/
         }
 
         private async void installsDropdown_SelectedIndexChanged(object sender, EventArgs e)
         {
-            currentInstall = await client.GetInstall(installsDropdown.Text);
-            client = null;
-            client = new MednaNetAPIClient.Client("localhost", "24215");
-            client.start(currentInstall.code);
+            /* currentInstall = await client.GetInstall(installsDropdown.Text);
+             client = null;
+             client = new MednaNetAPIClient.Client("localhost", "24215");
+             client.start(currentInstall.code);*/
         }
 
         private void button1_Click_1(object sender, EventArgs e)
