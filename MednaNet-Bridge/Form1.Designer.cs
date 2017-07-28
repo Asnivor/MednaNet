8c2880a29e85be45dfaab8b6134c966764437904
Stephen Burgess <stephen_burgess@hotmail.com>
2017-07-28 22:20:23 +0100

    Code tidy

--- MednaNet-Bridge/Form1.Designer.cs
+++ MednaNet-Bridge/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.botToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.botMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botToken
+            // 
+            this.botToken.Location = new System.Drawing.Point(12, 25);
+            this.botToken.Name = "botToken";
+            this.botToken.Size = new System.Drawing.Size(303, 20);
+            this.botToken.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bot Token";
+            // 
+            // botMessage
+            // 
+            this.botMessage.Location = new System.Drawing.Point(12, 113);
+            this.botMessage.Name = "botMessage";
+            this.botMessage.Size = new System.Drawing.Size(303, 20);
+            this.botMessage.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message From Bot";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.sendBotMessage);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(303, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start Bot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StartBot);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 303);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.botMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.botToken);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox botToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox botMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- MednaNet-Bridge/Form1.cs
+++ MednaNet-Bridge/Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Discord;
+using Discord.WebSocket;
 
 namespace MednaNet_Bridge
 {
     public partial class Form1 : Form
     {
+        private DiscordSocketClient client;
+        System.Timers.Timer r = new System.Timers.Timer(1000);
         public Form1()
         {
             InitializeComponent();
         }
+
+        private async void StartBot(object sender, EventArgs e)
+        {
+            await startBot();
+        }
+
+        private async Task startBot()
+        {
+            this.client = new DiscordSocketClient();
+            this.client.Log += Log;
+            this.client.MessageReceived += MessageReceived;
+
+            string token = botToken.Text;
+            await this.client.LoginAsync(TokenType.Bot, token);
+            await this.client.StartAsync();
+        }
+
+        private async void sendBotMessage(object sender, EventArgs e)
+        {
+            var ch = this.client.GetChannel(335445676227952640) as ISocketMessageChannel;
+            await ch.SendMessageAsync(botMessage.Text);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+
+           /* MedLaunchEntities db = new MedLaunchEntities();
+            var currentId = 0;
+            while (true)
+            {
+                // code here
+
+
+
+
+                var result = from q in db.messages
+                             where q.id > currentId
+                             orderby q.id ascending
+                             select q;
+
+                foreach (var s in result)
+                {
+                    currentId = s.id;
+
+                    var ch = _client.GetChannel(335445676227952640) as ISocketMessageChannel;
+                    await ch.SendMessageAsync("<" + s.name + "> " + s.message1);
+                }
+
+                Thread.Sleep(5000);
+            }*/
+        }
+
+       
     }
 }
